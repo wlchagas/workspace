a05c16e00d6474bb4a7b46b711db9db6cf36de98
Wellington Chagas <wlchagas@gmail.com>
2021-03-10 22:15:11 -0300

    Delete report_app directory

--- report_app/app/models/report.rb
+++
@@ -1,4 +0,0 @@
-class Report < ApplicationRecord
-    has_one :category, inverse_of: :report
-    accepts_nested_attributes_for :category, reject_if: :all_blank, allow_destroy: true
-end
--- report_app/app/views/reports/_report.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! report, :id, :name, :date, :unit, :created_at, :updated_at
-json.url report_url(report, format: :json)
